c8d387217a1b7b5700e93ccc7df43572fad12f57
Brian Barragan <bbarragan@bbarrag-ltmda6p.internal.salesforce.com>
2023-06-27 16:37:02 -0700

    ending the day

--- lifetracker-api/lifetracker.sql
+++ lifetracker-api/lifetracker.sql
@@ -1,21 +1,21 @@
-\echo 'Delete and recreate user db?'
+\echo 'Delete and recreate lifetracker db?'
 \prompt 'Return for yes or control-C to cancel > ' foo
 
-DROP DATABASE user;
-CREATE DATABASe user;
-\connect users;
+DROP DATABASE lifetracker;
+CREATE DATABASE lifetracker;
+\connect lifetracker;
 
 \i lifetracker-schema.sql
 
 
-\echo 'Delete and recreate nutrition db?'
-\prompt 'Return for yes or control-C to cancel > ' foo
+-- \echo 'Delete and recreate nutrition db?'
+-- \prompt 'Return for yes or control-C to cancel > ' foo
 
-DROP DATABASE nutrition;
-CREATE DATABASe nutrition;
-\connect nutrition;
+-- DROP DATABASE lifetracker_test;
+-- CREATE DATABASe nutrition;
+-- \connect nutrition;
 
-\i lifetracker-schema.sql
+-- \i lifetracker-schema.sql
 
 -- \echo 'Delete and recreate vaccine_hub_test db?'
 -- \prompt 'Return for yes or control-C to cancel > ' foo
