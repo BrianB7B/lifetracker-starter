8dfd98e6b3ead0cc6f0a0e32661a98e252e6c789
Brian Barragan <bbarragan@bbarrag-ltmda6p.internal.salesforce.com>
2023-07-06 09:39:53 -0700

    message for user if signed in

--- lifetracker-ui/src/components/ActivityPage/ActivityPage.jsx
+++ lifetracker-ui/src/components/ActivityPage/ActivityPage.jsx
@@ -1,11 +1,31 @@
+// import "./ActivityPage.css";
+
+
+// export default function Activity() {
+//   return (
+//     <div className="ActivityPage">
+//         <h1>Login to see your activity data.</h1>
+      
+//     </div>
+//   );
+// }
+import { useEffect, useState } from "react";
 import "./ActivityPage.css";
 
+export default function Activity({ isLoggedIn }) {
+  const [message, setMessage] = useState("");
 
-export default function Activity() {
+  useEffect(() => {
+    if (isLoggedIn) {
+      setMessage("You are logged in. Here is your activity data.");
+    } else {
+      setMessage("Login to see your activity data.");
+    }
+  }, [isLoggedIn]);
+
   return (
     <div className="ActivityPage">
-        <h1>Login to see your activity data.</h1>
-      
+      <h1>{message}</h1>
     </div>
   );
 }
--- lifetracker-ui/src/components/App/App.jsx
+++ lifetracker-ui/src/components/App/App.jsx
@@ -1,3 +1,44 @@
+// import { useState } from 'react'
+// import { BrowserRouter, Routes, Route } from "react-router-dom"
+// import './App.css'
+// import Home from '../Home/Home'
+// import Navbar from '../Navbar/Navbar'
+// import Register from '../Register/Register'
+// import Login from '../Login/Login'
+// import Portal from '../Portal/Portal'
+// import Activity from '../ActivityPage/ActivityPage'
+// import Exercise from '../ExercisePage/ExercisePage'
+// import Nutrition from '../NutritionPage/NutritionPage'
+// import Sleep from '../SleepPage/SleepPage'
+
+// export default function App() {
+//   const [appState, setAppState] = useState({})
+//   const isAuthenticated = Boolean(appState?.user?.email)
+//   const handleLogout = () => { 
+//     setAppState({});
+//   };
+
+//   return (
+//     <div className="App">
+//       <BrowserRouter>
+//         <Navbar isAuthenticated={isAuthenticated} handleLogout={handleLogout} />
+//         <Routes>
+//           <Route path="/" element={<Home />} />
+//           <Route path="/register" element={<Register setAppState={setAppState} />} />
+//           <Route path="/login" element={<Login setAppState={setAppState} />} />
+//           <Route
+//             path="/portal"
+//             element={<Portal user={appState?.user} setAppState={setAppState} />}
+//           />
+//           <Route path='/activity'element={<Activity/>}/>
+//           <Route path='/exercise'element={<Exercise/>}/>
+//           <Route path='/nutrition'element={<Nutrition/>}/>
+//           <Route path='/sleep'element={<Sleep/>}/>
+//         </Routes>
+//       </BrowserRouter>  
+//     </div>
+//   )
+// }
 import { useState } from 'react'
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import './App.css'
@@ -14,7 +55,7 @@ import Sleep from '../SleepPage/SleepPage'
 export default function App() {
   const [appState, setAppState] = useState({})
   const isAuthenticated = Boolean(appState?.user?.email)
-  const handleLogout = () => {
+  const handleLogout = () => { 
     setAppState({});
   };
 
@@ -30,12 +71,15 @@ export default function App() {
             path="/portal"
             element={<Portal user={appState?.user} setAppState={setAppState} />}
           />
-          <Route path='/activity'element={<Activity/>}/>
-          <Route path='/exercise'element={<Exercise/>}/>
-          <Route path='/nutrition'element={<Nutrition/>}/>
-          <Route path='/sleep'element={<Sleep/>}/>
+          <Route
+            path="/activity"
+            element={<Activity isLoggedIn={isAuthenticated} />}
+          />
+          <Route path="/exercise" element={<Exercise />} />
+          <Route path="/nutrition" element={<Nutrition />} />
+          <Route path="/sleep" element={<Sleep />} />
         </Routes>
       </BrowserRouter>  
     </div>
-  )
+  );
 }
