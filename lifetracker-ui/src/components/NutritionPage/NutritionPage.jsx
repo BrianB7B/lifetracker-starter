5cf3215b3f406a97b3c8e8f698ace44a2dc9a147
Brian Barragan <bbarragan@bbarrag-ltmda6p.internal.salesforce.com>
2023-07-06 14:50:50 -0700

    have the form for nutrition data

--- lifetracker-ui/src/components/NutritionPage/NutritionPage.jsx
+++ lifetracker-ui/src/components/NutritionPage/NutritionPage.jsx
@@ -1,20 +1,91 @@
-import "./NutritionPage.css"
-import { useEffect, useState } from "react";
+import { useState, useEffect } from "react";
+import "./NutritionPage.css";
 
 export default function Nutrition({ isLoggedIn }) {
   const [message, setMessage] = useState("");
+  const [foodName, setFoodName] = useState("");
+  const [foodType, setFoodType] = useState("");
+  const [quantity, setQuantity] = useState("");
+  const [calories, setCalories] = useState("");
+  const [imageUrl, setImageUrl] = useState("");
+  const [isSubmitted, setIsSubmitted] = useState(false);
+  const [nutritionLogs, setNutritionLogs] = useState([]);
 
   useEffect(() => {
     if (isLoggedIn) {
-      setMessage("You are logged in. Here is your nutrition data.");
+      setMessage("You are logged in. Add your nutrition data.");
     } else {
-      setMessage("Login to see your nutrition data.");
+      setMessage("Login to add your nutrition data.");
     }
-  }, [isLoggedIn]);http://localhost:5173/nutrition
+  }, [isLoggedIn]);
+
+  const handleAddNutrition = (event) => {
+    event.preventDefault();
+
+    const nutritionData = {
+      foodName,
+      foodType,
+      quantity,
+      calories,
+      imageUrl,
+    };
+
+    setNutritionLogs((prevLogs) => [...prevLogs, nutritionData]);
+
+    setFoodName("");
+    setFoodType("");
+    setQuantity("");
+    setCalories("");
+    setImageUrl("");
+
+    setIsSubmitted(true);
+  };
 
   return (
     <div className="NutritionPage">
-      <h1>{message}</h1>
+      <h2>{message}</h2>
+
+      {isLoggedIn && (
+        <div>
+          <form onSubmit={handleAddNutrition}>
+          <label htmlFor="foodName">Food Name:</label>
+            <input type="text" name="foodName" id="foodName" value={foodName} onChange={(e) => setFoodName(e.target.value)} required />
+
+            <label htmlFor="foodType">Food Type:</label>
+            <select name="foodType" id="foodType" value={foodType} onChange={(e) => setFoodType(e.target.value)}>
+              <option value="snack">Snack</option>
+              <option value="food">Food</option>
+              <option value="beverage">Beverage</option>
+            </select>
+
+            <label htmlFor="quantity">Quantity:</label>
+            <input type="number" name="quantity" id="quantity" value={quantity} onChange={(e) => setQuantity(parseInt(e.target.value))} />
+
+            <label htmlFor="calories">Calories:</label>
+            <input type="number" name="calories" id="calories" value={calories} onChange={(e) => setCalories(parseInt(e.target.value))} />
+
+            <label htmlFor="imageUrl">Image URL:</label>
+            <input type="text" name="imageUrl" id="imageUrl" value={imageUrl} onChange={(e) => setImageUrl(e.target.value)} />
+
+            <button type="submit">Add Nutrition</button>
+          </form>
+
+
+          <div className="previousLogs">
+            <h2>Nutrition Data</h2>
+            {nutritionLogs.map((log, index) => (
+              <div key={index}>
+                 <p>Food Name: {log.foodName}</p>
+                  <p>Food Type: {log.foodType}</p>
+                  <p>Quantity: {log.quantity}</p>
+                  <p>Calories: {log.calories}</p>
+                  <p>Image URL: {log.imageUrl}</p>
+                  <hr />
+              </div>
+            ))}
+          </div>
+        </div>
+      )}
     </div>
   );
 }
