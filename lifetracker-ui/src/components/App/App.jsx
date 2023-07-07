16ec409ec7f306b7798d02c360c9d0b41557a27f
Brian Barragan <bbarragan@bbarrag-ltmda6p.internal.salesforce.com>
2023-07-06 21:52:58 -0700

    working token

--- lifetracker-ui/src/components/App/App.jsx
+++ lifetracker-ui/src/components/App/App.jsx
@@ -30,34 +30,50 @@
 //             path="/portal"
 //             element={<Portal user={appState?.user} setAppState={setAppState} />}
 //           />
-//           <Route path='/activity'element={<Activity/>}/>
-//           <Route path='/exercise'element={<Exercise/>}/>
-//           <Route path='/nutrition'element={<Nutrition/>}/>
-//           <Route path='/sleep'element={<Sleep/>}/>
+//           <Route
+//             path="/activity"element={<Activity isLoggedIn={isAuthenticated} />} />
+//           <Route path="/exercise" element={<Exercise isLoggedIn={isAuthenticated}/>}  />
+//           <Route path="/nutrition" element={<Nutrition isLoggedIn={isAuthenticated} />} />
+//           <Route path="/sleep" element={<Sleep isLoggedIn={isAuthenticated}/>} />
 //         </Routes>
 //       </BrowserRouter>  
 //     </div>
-//   )
+//   );
 // }
-import { useState } from 'react'
-import { BrowserRouter, Routes, Route } from "react-router-dom"
-import './App.css'
-import Home from '../Home/Home'
-import Navbar from '../Navbar/Navbar'
-import Register from '../Register/Register'
-import Login from '../Login/Login'
-import Portal from '../Portal/Portal'
-import Activity from '../ActivityPage/ActivityPage'
-import Exercise from '../ExercisePage/ExercisePage'
-import Nutrition from '../NutritionPage/NutritionPage'
-import Sleep from '../SleepPage/SleepPage'
 
-export default function App() {
-  const [appState, setAppState] = useState({})
-  const isAuthenticated = Boolean(appState?.user?.email)
-  const handleLogout = () => { 
+import { useState, useEffect } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import './App.css';
+import Home from '../Home/Home';
+import Navbar from '../Navbar/Navbar';
+import Register from '../Register/Register';
+import Login from '../Login/Login';
+import Portal from '../Portal/Portal';
+import Activity from '../ActivityPage/ActivityPage';
+import Exercise from '../ExercisePage/ExercisePage';
+import Nutrition from '../NutritionPage/NutritionPage';
+import Sleep from '../SleepPage/SleepPage';
+
+function App() {
+  const [appState, setAppState] = useState(() => {
+    const storedUser = localStorage.getItem('user');
+    return storedUser ? JSON.parse(storedUser) : {};
+  });
+
+  const isAuthenticated = Boolean(appState?.user?.email);
+
+  const handleLogout = () => {
+    localStorage.removeItem('user');
     setAppState({});
   };
+
+  useEffect(() => {
+    if (isAuthenticated) {
+      localStorage.setItem('user', JSON.stringify(appState));
+    } else {
+      localStorage.removeItem('user');
+    }
+  }, [isAuthenticated, appState]);
 
   return (
     <div className="App">
@@ -71,13 +87,14 @@ export default function App() {
             path="/portal"
             element={<Portal user={appState?.user} setAppState={setAppState} />}
           />
-          <Route
-            path="/activity"element={<Activity isLoggedIn={isAuthenticated} />} />
-          <Route path="/exercise" element={<Exercise isLoggedIn={isAuthenticated}/>}  />
+          <Route path="/activity" element={<Activity isLoggedIn={isAuthenticated} />} />
+          <Route path="/exercise" element={<Exercise isLoggedIn={isAuthenticated} />} />
           <Route path="/nutrition" element={<Nutrition isLoggedIn={isAuthenticated} />} />
-          <Route path="/sleep" element={<Sleep isLoggedIn={isAuthenticated}/>} />
+          <Route path="/sleep" element={<Sleep isLoggedIn={isAuthenticated} />} />
         </Routes>
-      </BrowserRouter>  
+      </BrowserRouter>
     </div>
   );
 }
+
+export default App;
