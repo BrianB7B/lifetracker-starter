885ef17308d423e60af99036a83eda53354026f0
Brian Barragan <bbarragan@bbarrag-ltmda6p.internal.salesforce.com>
2023-07-07 23:35:36 -0700

    readme

--- lifetracker-api/config.js
+++ lifetracker-api/config.js
@@ -16,7 +16,7 @@ function getDatabaseUri() {
   const dbProdName = process.env.DATABASE_NAME || "vaccine_hub"
   const dbName = process.env.NODE_ENV === "test" ? dbTestName : dbProdName
 
-  return "postgres://lifetracker_qaar_user:dZfuDYy4XEZHiO5E5HMunY49GnM6qYIo@dpg-cikfdgtgkuvinfisonj0-a/lifetracker_qaar";
+  return process.env.DATABASE_URL || `postgresql://${dbUser}:${dbPass}@${dbHost}:${dbPort}/${dbName}`
 
 }
 
@@ -34,3 +34,40 @@ module.exports = {
   BCRYPT_WORK_FACTOR,
   getDatabaseUri,
 }
+
+// "use strict"
+
+// require("dotenv").config()
+// require("colors")
+
+// const PORT = process.env.PORT ? Number(process.env.PORT) : 3001
+// const IS_TESTING = process.env.NODE_ENV === "test"
+
+// // Use dev database, testing database, or via env var, production database
+// function getDatabaseUri() {
+//   const dbUser = process.env.DATABASE_USER || "postgres"
+//   const dbPass = process.env.DATABASE_PASS ? encodeURI(process.env.DATABASE_PASS) : "postgres"
+//   const dbHost = process.env.DATABASE_HOST || "local"
+//   const dbPort = process.env.DATABASE_PORT || 5432
+//   const dbTestName = process.env.DATABASE_TEST_NAME || "vaccine_hub_test"
+//   const dbProdName = process.env.DATABASE_NAME || "vaccine_hub"
+//   const dbName = process.env.NODE_ENV === "test" ? dbTestName : dbProdName
+
+//   return "postgres://lifetracker_qaar_user:dZfuDYy4XEZHiO5E5HMunY49GnM6qYIo@dpg-cikfdgtgkuvinfisonj0-a/lifetracker_qaar";
+
+// }
+
+// const BCRYPT_WORK_FACTOR = IS_TESTING ? 1 : 13
+
+// console.log("Vaccine Hub Config:".red)
+// console.log("PORT:".blue, PORT)
+// console.log("BCRYPT_WORK_FACTOR".blue, BCRYPT_WORK_FACTOR)
+// console.log("Database:".blue, getDatabaseUri())
+// console.log("---")
+
+// module.exports = {
+//   PORT,
+//   IS_TESTING,
+//   BCRYPT_WORK_FACTOR,
+//   getDatabaseUri,
+// }
